465620059cbc15b7c7ddef7501f4c3e7f2c7eff0
Brynne Wilkinson <brynneewilkinson13@gmail.com>
2021-03-08 12:46:13 -0500

    Final with Comments

--- split.py
+++ split.py
@@ -9,7 +9,7 @@ from typing import Iterator, List
 import random
 
 
-
+# generator function
 def read_tags(path: str) -> Iterator[List[List[str]]]:
     with open(path, "r") as source:
         lines = []
@@ -29,20 +29,27 @@ def read_tags(path: str) -> Iterator[List[List[str]]]:
 def main(args: argparse.Namespace) -> None:
     # TODO: do the work here.
     print(args)
+    #define input
     corpus = list(read_tags(args.input))
 
+    # seed
     random.seed(args.seed)
 
+    # shuffle the corpus
     makeitfun = random.shuffle(corpus)
+    
+    # define size of corpus and prepare indices
     size = len(corpus)
     
     eightypercent = int(size * .8)
     ninetypercent = int(size * .9)
+    
+    # split the shuffled data
     trainslice = corpus[:eightypercent]
     devslice = corpus[eightypercent:ninetypercent]
     testslice = corpus[ninetypercent:]
     
-    
+    #write the data to the respective files
     with open(args.train, 'w') as sink:
         for item in trainslice:
             for detail in item:
@@ -65,11 +72,11 @@ def main(args: argparse.Namespace) -> None:
 if __name__ == "__main__":
     # TODO: declare arguments.
     parser = argparse.ArgumentParser()
-    parser.add_argument("--seed", type=int, required=True, help="path to write the training data to")
+    parser.add_argument("--seed", type=int, required=True)
     parser.add_argument("input", help="path to write the file to")
-    parser.add_argument("train", help="path to write the training data to")
-    parser.add_argument("dev", help="path to write the dev data to")
-    parser.add_argument("test", help="path to write the test data to")
+    parser.add_argument("train", help="file to write the training data to")
+    parser.add_argument("dev", help="file to write the dev data to")
+    parser.add_argument("test", help="file to write the test data to")
     # TODO: parse arguments and pass them to `main`.
     main(parser.parse_args())
     ...
